b1be82b9a0bb0f865f95dc5c404261e4263d311d
unvireak <dydy11v@gmail.com>
2024-06-09 23:32:05 +0700

    edit screen

--- snapKitLayout/SceneDelegate.swift
+++ snapKitLayout/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             guard let windowScene = (scene as? UIWindowScene) else { return }
         
             let window = UIWindow(windowScene: windowScene)
-            let navigationController = UINavigationController(rootViewController: TableViewController())
+            let navigationController = UINavigationController(rootViewController: CreateNoteViewController())
             window.rootViewController = navigationController
             window.makeKeyAndVisible()
             self.window = window
--- snapKitLayout/TableViewController.swift
+++ snapKitLayout/TableViewController.swift
@@ -14,6 +14,7 @@ class TableViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
         tableView.dataSource = self
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         
