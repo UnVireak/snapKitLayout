69715b5adcbf78b1945fe8b69f70ca77237c3a3a
unvireak <dydy11v@gmail.com>
2024-06-11 23:58:55 +0700

    delete func

--- snapKitLayout/TableViewFolderController.swift
+++ snapKitLayout/TableViewFolderController.swift
@@ -86,6 +86,55 @@ extension TableViewFolderController: UITableViewDataSource{
     
 }
 extension TableViewFolderController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete"){ [weak self] _, _, complete in
+            self?.confirmDeleteItem(at: indexPath) { confirmed in
+                if confirmed {
+                    self?.deleteFolder(at: indexPath)
+                }
+                complete(confirmed)
+            }
+            
+        }
+        
+        let editAction = UIContextualAction(style: .normal, title: "Edit") { [weak self] _, _, complete in
+            
+            self?.editdata(at: indexPath)
+            complete(true)
+        }
+        editAction.backgroundColor = UIColor(red: 0/255, green: 127/255, blue: 255/255, alpha: 1.0)
+        
+        let config = UISwipeActionsConfiguration(actions: [deleteAction, editAction])
+        config.performsFirstActionWithFullSwipe = false
+        return config
+    }
+    func confirmDeleteItem(at indexPath: IndexPath, completion: @escaping (Bool) -> Void) {
+         let alertController = UIAlertController(title: "Confirm Deletion", message: "Are you sure you want to delete this folder?", preferredStyle: .alert)
+         
+         let confirmAction = UIAlertAction(title: "Delete", style: .destructive) { _ in
+             completion(true)
+         }
+         
+         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { _ in
+             completion(false)
+         }
+         
+         alertController.addAction(confirmAction)
+         alertController.addAction(cancelAction)
+         
+         present(alertController, animated: true, completion: nil)
+     }
+    
+    func deleteFolder(at indexPath: IndexPath) {
+        DataManager.shared.removeFolder(at: indexPath.item)
+        folderNote.remove(at: indexPath.item)
+        tableView.deleteRows(at: [indexPath], with: .none)
+    }
     
 }
 
